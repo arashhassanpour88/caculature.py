05ca26ef184cd852edb6ab90b96fc8e331b03c87
arash hassanpour <arash.hsnp@gmail.com>
2024-07-13 20:00:49 +0330

    Add files via upload

---
+++ calculature.py
@@ -0,0 +1,17 @@
+a=(int(input("enter a number: ")))
+s=(input("opratore: "))
+d=(int(input("enter another number: ")))
+if s=="*":
+    print(a*d)
+if s=="+":
+    print(a+d)
+if s=="-":
+    print(a-d)
+if s=="/":
+    print(a/d)
+if s=="//":
+    print(a//d)
+if s=="%":
+    print(a%d)
+if s=="**":
+    print(a**d)
